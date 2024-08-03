971486fe5eb0fbaed5be2f27b5a454ad4b58cbda
Weslley <weslley.c.j.4@gmail.com>
2024-08-03 20:54:32 -0300

    adicionando campos e elementos

--- App.tsx
+++ App.tsx
@@ -1,24 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { LoginScreen } from './src/screens/login';
+import imageBg from './src/imgs/bg.png'
+import { Input, Button} from '@rneui/base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Olá, Mundo!</Text>
-      <StatusBar style="auto" />
+    <ImageBackground source={imageBg} style={styles.imagemFundo}>
+      <View style={styles.container}>
 
-      <Button title="Clique-me" onPress={() =>
-        console.log('Clicou')}></Button>
-    </View>
+        <Text style={styles.logo}>APP</Text>        
+
+        {/* Campos de Input */}
+        <Input placeholder='Digite seu email'
+        keyboardType={'email-address'} leftIcon={{name: 'person', color: 'white'}}
+        inputStyle={{color: 'white'}} inputContainerStyle={styles.containerInput}
+        />
+
+        <Input placeholder='Digite sua senha' secureTextEntry={true}
+        leftIcon={{name: 'lock', color: 'white'}} inputStyle={{color: 'white'}}
+        inputContainerStyle={styles.containerInput}
+        />
+
+      </View>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
+const styles = StyleSheet.create ({
+  logo: { fontSize: 30, color: 'white', textAlign: 'center' },
+  imagemFundo: { height: '100%', width: '100%' },
+  container: { flex: 1, justifyContent: 'center', padding: 10, alignItems: 'stretch' },
+  containerInput: { backgroundColor: 'rgba(255, 255, 255, 0.3', borderRadius: 30, }
 });
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export interface LoginscreenProps {}
+
+export function LoginScreen ( props: LoginscreenProps ) {
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+        </View>
+    );
+}
