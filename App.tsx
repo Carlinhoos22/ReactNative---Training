9317f3039e381169a611ec3417b751b4cdfa8d26
Weslley <weslley.c.j.4@gmail.com>
2024-08-04 21:09:36 -0300

    adicionando mais elementos

--- App.tsx
+++ App.tsx
@@ -14,13 +14,26 @@ export default function App() {
         {/* Campos de Input */}
         <Input placeholder='Digite seu email'
         keyboardType={'email-address'} leftIcon={{name: 'person', color: 'white'}}
-        inputStyle={{color: 'white'}} inputContainerStyle={styles.containerInput}
+        inputStyle={{color: 'black'}} labelStyle={{color: 'white'}}
+        inputContainerStyle={{
+          backgroundColor: 'rgba(255, 255, 255, 0.3)',
+          borderRadius: 10,
+        }}
         />
 
         <Input placeholder='Digite sua senha' secureTextEntry={true}
         leftIcon={{name: 'lock', color: 'white'}} inputStyle={{color: 'white'}}
-        inputContainerStyle={styles.containerInput}
+        inputContainerStyle={{
+          backgroundColor: 'rgba(255, 255, 255, 0.3)',
+          borderRadius: 10,          
+        }}
         />
+
+        <Button title="LOGAR" containerStyle={{borderRadius: 30}} raised={true}/>
+
+        <Text style={styles.botaoLogin}>
+          Não possui conta? Clique aqui para cadastra-se
+        </Text>
 
       </View>
     </ImageBackground>
@@ -31,5 +44,6 @@ const styles = StyleSheet.create ({
   logo: { fontSize: 30, color: 'white', textAlign: 'center' },
   imagemFundo: { height: '100%', width: '100%' },
   container: { flex: 1, justifyContent: 'center', padding: 10, alignItems: 'stretch' },
-  containerInput: { backgroundColor: 'rgba(255, 255, 255, 0.3', borderRadius: 30, }
+  containerInput: { backgroundColor: 'rgba(255, 255, 255, 0.3', borderRadius: 30 },
+  botaoLogin: {color: 'white', fontSize: 20, textDecorationLine: 'underline', margin: 30, textAlign: 'center'}
 });
