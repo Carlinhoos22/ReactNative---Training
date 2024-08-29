cefd6e29a84c4b81e767e843739b29485c0f448c
Weslley <weslley.c.j.4@gmail.com>
2024-08-28 23:58:48 -0300

    atualizando

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import { LoginScreen } from './src/screens/login';
-import imageBg from './src/imgs/bg.png'
+import imageBg from './src/imgs/new.jpg'
 import { Input, Button} from '@rneui/base';
 
 export default function App() {
@@ -9,12 +9,12 @@ export default function App() {
     <ImageBackground source={imageBg} style={styles.imagemFundo}>
       <View style={styles.container}>
 
-        <Text style={styles.logo}>APP</Text>        
+        <Text style={styles.logo}>TELA LOGIN</Text>        
 
         {/* Campos de Input */}
         <Input placeholder='Digite seu email'
         keyboardType={'email-address'} leftIcon={{name: 'person', color: 'white'}}
-        inputStyle={{color: 'black'}} labelStyle={{color: 'white'}}
+        inputStyle={{color: 'white'}}
         inputContainerStyle={{
           backgroundColor: 'rgba(255, 255, 255, 0.3)',
           borderRadius: 10,
@@ -44,6 +44,6 @@ const styles = StyleSheet.create ({
   logo: { fontSize: 30, color: 'white', textAlign: 'center' },
   imagemFundo: { height: '100%', width: '100%' },
   container: { flex: 1, justifyContent: 'center', padding: 10, alignItems: 'stretch' },
-  containerInput: { backgroundColor: 'rgba(255, 255, 255, 0.3', borderRadius: 30 },
+  containerInput: { backgroundColor: 'rgba(255, 255, 255, 0.3', borderRadius: 30, },
   botaoLogin: {color: 'white', fontSize: 20, textDecorationLine: 'underline', margin: 30, textAlign: 'center'}
 });
